21630bd0c2cc91f460dbb8a5d6801f9b3b4602d2
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2024-10-15 11:35:05 -0300

    Update WeatherInformations5Days.jsx

--- react-previsao/src/components/WeatherInformation5Days/WeatherInformations5Days.jsx
+++ react-previsao/src/components/WeatherInformation5Days/WeatherInformations5Days.jsx
@@ -2,17 +2,40 @@
 import './WeatherInformation5Days.css'
 
 function WeatherInformations5Days({ weather5Days }) {
-    // Corrigido de weather5days para weather5Days
-    if (!weather5Days || !weather5Days.weather || weather5Days.weather.length === 0) {
-        // Retorna uma mensagem de carregamento ou outra mensagem apropriada enquanto os dados não chegam
-        return <div>Esperando Informações...</div>;
-    }
 
     console.log(weather5Days);
 
+    let dailyForecast = {}
+
+    for(let forecast of weather5Days.list){
+        const date = new Date(forecast.dt * 1000).toLocaleDateString()
+
+        if(!dailyForecast[date]){
+            dailyForecast[date] = forecast
+        }
+
+    }
+    
+    const next5DaysForecast = Object.values(dailyForecast).slice(1,6)
+
+    function convertDate(date){
+        const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', {weekday: 'long', day:'2-digit'})
+        
+        return newDate
+    }
+
+
     return (
         <div className='weather-container'>
-            <p>5 Days</p>
+            <h3>Próximos Dias</h3>
+            {next5DaysForecast.map(forecast => (
+                <div key={forecast.dt}>
+                    <p>{convertDate(forecast)}</p>
+                    <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}></img>
+                    <p>{forecast.weather[0].description}</p>
+                    <p>{Math.round(forecast.main.temp_max)}ºC</p>
+                </div>
+            ))}
         </div>
     );
 }
